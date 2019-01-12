58eebeec42b5ce487c023ffbc57cd67aadb49091
Ainslie Vaux <ainslie.vaux@gmail.com>
2019-01-12 17:23:47 -0500

    Fix compatibility with upstream classic-db v1.10

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2733_01_mangos_playercreate_skills_vanilla` bit(1) DEFAULT NULL
+  `required_z2732_01_mangos_seal_of_righteousness_cleanup` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
