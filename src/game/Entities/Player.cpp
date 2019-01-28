323d426877fced472c892ce6c53952257da0773f
Ainslie Vaux <ainslie.vaux@gmail.com>
2019-01-28 16:05:35 -0500

    Get melee crit rate a la Nostalrius core. Fixes low-level crit rate being much too low.

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -4583,25 +4583,46 @@ uint32 Player::GetShieldBlockValue() const
 
 float Player::GetMeleeCritFromAgility() const
 {
-    // from mangos 3462 for 1.12
-    float val = 0.0f, classrate = 0.0f;
+    float valLevel1 = 0.0f;
+    float valLevel60 = 0.0f;
     // critical
     switch (getClass())
     {
-        case CLASS_PALADIN: classrate = 19.77f; break;
-        case CLASS_SHAMAN:  classrate = 19.7f;  break;
-        case CLASS_MAGE:    classrate = 19.44f; break;
-        case CLASS_ROGUE:   classrate = 29.0f;  break;
-        case CLASS_HUNTER:  classrate = 53.0f;  break;      // in 2.0.x = 33
-        case CLASS_PRIEST:
-        case CLASS_WARLOCK:
+        case CLASS_PALADIN:
+        case CLASS_SHAMAN:
         case CLASS_DRUID:
+            valLevel1 = 4.6f;
+            valLevel60 = 20.0f;
+            break;
+        case CLASS_MAGE:
+            valLevel1 = 12.9f;
+            valLevel60 = 20.0f;
+            break;
+        case CLASS_ROGUE:
+            valLevel1 = 2.2f;
+            valLevel60 = 29.0f;
+            break;
+        case CLASS_HUNTER:
+            valLevel1 = 3.5f;
+            valLevel60 = 53.0f;
+            break;
+        case CLASS_PRIEST:
+            valLevel1 = 11.0f;
+            valLevel60 = 20.0f;
+            break;
+        case CLASS_WARLOCK:
+            valLevel1 = 8.4f;
+            valLevel60 = 20.0f;
+            break;
         case CLASS_WARRIOR:
-        default:            classrate = 20.0f; break;
+            valLevel1 = 3.9f;
+            valLevel60 = 20.0f;
+            break;
+        default:
+            return 0.0f;
     }
-
-    val = GetStat(STAT_AGILITY) / classrate;
-    return val;
+    float classrate = valLevel1 * float(60.0f - getLevel()) / 59.0f + valLevel60 * float(getLevel() - 1.0f) / 59.0f;
+    return GetStat(STAT_AGILITY) / classrate;
 }
 
 // Static multipliers for converting agi into dodge chance
